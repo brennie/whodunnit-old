ea366472d88b83d10db12bf14069fecd33ea9c1c
Barret Rennie <barret@brennie.ca>
2016-08-01 04:51:34 -0600

    Add a gulpfile
    The gulpfile currently has three tasks:
    
    * lint:css, for running stylelint on all CSS;
    * lint:js, for running eslint on all JS; and
    * lint, for running both of the above.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp');
+
+
+gulp.task('lint:css', () => {
+  const stylelint = require('gulp-stylelint');
+
+  return gulp.src(['src/**.css'])
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true},
+      ],
+    }));
+});
+
+
+gulp.task('lint:js', () => {
+  const eslint = require('gulp-eslint');
+  const eslintConfig = require('./.eslintrc.js');
+
+  return gulp.src(['src/**.js', 'src/**.jsx'])
+    .pipe(eslint(eslintConfig))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+
+gulp.task('lint', ['lint:js', 'lint:css']);
