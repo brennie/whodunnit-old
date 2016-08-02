ec58b293ffc3d0816ec9d8a7b9e9bfa70187194b
Barret Rennie <barret@brennie.ca>
2016-08-01 18:32:49 -0600

    Separate .eslintrc.js into server and client configs
    The .eslintrc.js has been split into two configurations:
    
    * src/client/.eslintrc.js, for linting client code; and
    * src/server/.eslintrc.js, for linting server code.
    
    This has been done because the server and client will have access to
    different things, such as React (client-only).

--- gulpfile.js
+++ gulpfile.js
@@ -17,15 +17,27 @@ gulp.task('lint:css', () => {
 });
 
 
-gulp.task('lint:js', () => {
+gulp.task('lint:js:server', () => {
   const eslint = require('gulp-eslint');
-  const eslintConfig = require('./.eslintrc.js');
+  const eslintConfig = require('./src/server/.eslintrc.js');
 
-  return gulp.src(['src/**.js', 'src/**.jsx'])
+  return gulp.src(['src/server/**.js'])
     .pipe(eslint(eslintConfig))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 
+gulp.task('lint:js:client', () => {
+  const eslint = require('gulp-eslint');
+  const eslintConfig = require('./src/client/.eslintrc.js');
+
+  return gulp.src(['src/client/**.js', 'src/client/**.jsx'])
+    .pipe(eslint(eslintConfig))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+})
+
+
+gulp.task('lint:js', ['lint:js:client', 'lint:js:server']);
 gulp.task('lint', ['lint:js', 'lint:css']);
