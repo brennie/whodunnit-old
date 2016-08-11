38a976bec4bbcde7f550585e45ebc23601b6ae77
Barret Rennie <barret@brennie.ca>
2016-08-11 14:34:35 -0600

    Clean up CSS

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Router from './components/router';
 import './css/reset.css';
+import './style.css';
 
 
 document.addEventListener('DOMContentLoaded', () => {
