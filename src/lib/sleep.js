abc01f19e1fcfcae0fd1ae1f9e1656f61c9c5c47
Barret Rennie <barret@brennie.ca>
2016-08-11 23:10:38 -0600

    Add a Promise wrapper around setTimeout

---
+++ src/lib/sleep.js
@@ -0,0 +1,10 @@
+/**
+ * Create a promise that will be resolved after the specified duration.
+ *
+ * @param {number=0} time The time (in milliseconds) to wait.
+ *
+ * @returns {Promise} A promise that is resolved after `time` milliseconds.
+ */
+const sleep = (time=0) => new Promise(r => setTimeout(r, time));
+
+export default sleep;
