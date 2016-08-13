544d50f560ca4cf6acadad06ccf9877f44b9ab34
Barret Rennie <barret@brennie.ca>
2016-08-12 19:12:51 -0600

    Change user.salt to be a binary field

--- migrations/001_user.js
+++ migrations/001_user.js
@@ -1,9 +1,8 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
     table.string('email').unique();
-    table.string('salt', 8);
+    table.binary('salt', 8);
     table.string('passHash', 64);
     table.string('name');
   });
