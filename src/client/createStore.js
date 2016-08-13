ced4cd0c27bd13c4759d61366276031af1d3c344
Barret Rennie <barret@brennie.ca>
2016-08-12 23:13:50 -0600

    Only allow the Redux DevTools extension in dev mode

---
+++ src/client/createStore.js
@@ -0,0 +1,38 @@
+/* global process */
+import {createStore as createReduxStore, compose} from 'redux';
+
+let createStore;
+
+/* Expose the appropriate `createStore` method on production and development.
+ *
+ * When built with webpack, only one branch will be compiled. This way
+ * devtools won't be enabled on production builds.
+ */
+if (process.env.NODE_ENV === 'production') {
+  createStore = createReduxStore;
+} else {
+  /**
+   * Create the store for development use.
+   *
+   * This will load the Redux DevTools extension if it is installed.
+   *
+   * @see https://github.com/zalmoxisus/redux-devtools-extension
+   * @see https://github.com/reactjs/redux/blob/master/docs/api/createStore.md
+   *
+   * @param {function} reducer The store reducer.
+   * @param {[]Object} [middleware] The middleware to use.
+   * @param {function} [enhancer] The store enhancer, such as middleware.
+   *
+   * @returns {Object} The store.
+   */
+  createStore = (reducer, initialState, enhancer) => createReduxStore(
+    reducer,
+    initialState,
+    compose(
+      enhancer,
+      window.devToolsExtension ? window.devToolsExtension() : e => e
+    )
+  );
+}
+
+export default createStore;
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import messageList from './components/message-list/reducers';
 import registerForm from './components/register-form/reducers';
 import Router from './components/router';
+import createStore from './createStore';
 import './css/reset.css';
 import './style.css';
 
 
-const reducers = combineReducers({
+const reducer = combineReducers({
   messageList,
   registerForm
 });
 
-const state = createStore(
-  reducers,
-  window.devToolsExtension && window.devToolsExtension(),
-  applyMiddleware(
-    thunkMiddleware
-  )
-);
+const state = createStore(reducer, {}, applyMiddleware(thunkMiddleware));
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
