5da052be7395a7a1aa68118770a0dfa704bce9c4
Barret Rennie <barret@brennie.ca>
2016-08-17 04:04:22 -0600

    De-nest CSS. Reorganize form css under components/ui/form

--- src/client/components/ui/form/index.js
+++ src/client/components/ui/form/index.js
@@ -2,3 +2,5 @@ export {default as ErrorList} from './errorList';
 export {default as Field} from './field';
 export {default as Form} from './form';
 export {default as Label} from './label';
+
+import './style.css';
